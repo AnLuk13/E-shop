ca15b532c8bfbae1e6118994504ac564d85fe30f
Antonio <45819634+AnLuk13@users.noreply.github.com>
2023-07-03 17:27:36 +0300

    Add form validation, svg icon for the registration page and create a submission file for data

---
+++ app.js
@@ -0,0 +1,13 @@
+const searchParams = new URLSearchParams(window.location.search);
+
+// Retrieve the form data from the query parameters
+const email = searchParams.get("email");
+const name = searchParams.get("name");
+const lastName = searchParams.get("last-name");
+const password = searchParams.get("password");
+
+// Populate the table with the form data
+document.getElementById("email").textContent = email;
+document.getElementById("name").textContent = name;
+document.getElementById("lastName").textContent = lastName;
+document.getElementById("password").textContent = password;
